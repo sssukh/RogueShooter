97ccb80b9ba9f321fde5571fc27f5a4017261f5b
sssukh <sssukh50@gmail.com>
2025-07-24 23:20:10 +0900

    UW_GameMenu On Progress
    UW_GameMenu 컨버팅 중. 세션 관련 내용 학습 필요

---
+++ Source/RogueShooter/Private/UI/UW_GameMenu.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/UW_GameMenu.h"
+
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+UUW_GameMenu::UUW_GameMenu(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+void UUW_GameMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	Button_Continue->OnClicked.AddDynamic(this,&UUW_GameMenu::OnClicked_Continue);
+	Button_QuitDesktop->OnClicked.AddDynamic(this,&UUW_GameMenu::OnClicked_QuitDesktop);
+	Button_QuitMenu->OnClicked.AddDynamic(this,&UUW_GameMenu::OnClicked_QuitMenu);
+}
+
+void UUW_GameMenu::OnClicked_Continue()
+{
+	RemoveFromParent();
+
+	if(OnMenuClosed.IsBound())
+		OnMenuClosed.Broadcast();
+}
+
+void UUW_GameMenu::OnClicked_QuitDesktop()
+{
+	UKismetSystemLibrary::QuitGame(GetWorld(),nullptr,EQuitPreference::Quit,false);
+}
+
+void UUW_GameMenu::OnClicked_QuitMenu()
+{
+	UGameplayStatics::OpenLevel(GetWorld(),FName(TEXT("M_MainMenu")));
+
+
+	// TODO : Destroy session
+	// UGameplayStatics::GetPlayerController(GetWorld(),0)->session
+}
---
+++ Source/RogueShooter/Public/UI/UW_GameMenu.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UW_GameMenu.generated.h"
+
+DECLARE_MULTICAST_DELEGATE(FOnMenuClosed);
+
+class UButton;
+/**
+ * 
+ */
+UCLASS()
+class ROGUESHOOTER_API UUW_GameMenu : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UUW_GameMenu(const FObjectInitializer& ObjectInitializer);
+
+	 void NativeConstruct() override;
+
+	UFUNCTION()
+	void OnClicked_Continue();
+
+	UFUNCTION()
+	void OnClicked_QuitDesktop();
+
+	UFUNCTION()
+	void OnClicked_QuitMenu();
+public:
+	UPROPERTY(BlueprintReadWrite, Category="UW_GameMenu|Designer", meta=(BindWidget))
+	TObjectPtr<UButton> Button_Continue;
+
+	UPROPERTY(BlueprintReadWrite, Category="UW_GameMenu|Designer", meta=(BindWidget))
+	TObjectPtr<UButton> Button_QuitDesktop;
+	
+	UPROPERTY(BlueprintReadWrite, Category="UW_GameMenu|Designer", meta=(BindWidget))
+	TObjectPtr<UButton> Button_QuitMenu;
+
+	UPROPERTY(BlueprintAssignable,BlueprintCallable)
+	FOnMenuClosed OnMenuClosed;
+};
